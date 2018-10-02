652d1c0270b57c2c2dcb3e4ebe676b2200dd8a13
Linda Ng <linhnb1690@gmail.com>
2018-10-02 10:05:12 +0700

    add flask files

---
+++ dockers/buildflask.pp
@@ -0,0 +1,10 @@
+class docker::buildflask {
+  docker::image { 'flask':
+   docker_file => '/flask-app/Dockerfile',
+   notify => Docker::Run['flask'],
+  }
+
+  docker::run { 'flask':
+   image => 'flask',
+  }
+}
